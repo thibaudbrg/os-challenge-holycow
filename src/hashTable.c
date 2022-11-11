dc251afa4006bc2023f2c0cd644f2d68e2fcaa01
onsriahi14 <73482468+onsriahi14@users.noreply.github.com>
2022-11-11 20:42:20 +0100

    improving the code

---
+++ src/hashTable.c
@@ -0,0 +1,27 @@
+
+#include "../include/hashTable.h"
+#include "request.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MAX 256
+
+uint64_t  hashTable[MAX] = {0};
+
+uint8_t index_hash (uint8_t *hash){
+    uint8_t  i =0;
+    for(int j =0; j< SIZE_HASH; ++j){
+        i += hash[j];
+    }
+    return i;
+}
+
+void insert(uint8_t *hash, uint64_t answer){
+    int i = index_hash(hash);
+    hashTable[i] = answer;
+}
+
+uint64_t search(uint8_t *hash){
+    return hashTable[index_hash(hash)];
+}
