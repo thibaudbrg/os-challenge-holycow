b0e7f6d5032c157b293c21b592c47888b1f43887
onsriahi14 <73482468+onsriahi14@users.noreply.github.com>
2022-11-11 20:48:34 +0100

    Update request.h

--- include/request.h
+++ include/request.h
@@ -1,17 +1,22 @@
 #pragma once
-
+#include <stddef.h>
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+#include <stdint.h>
 #define SIZE_HASH 32
 
 typedef struct {
     uint8_t *hash;
-    u_int64_t start;
-    u_int64_t end;
+    uint64_t start;
+    uint64_t end;
     uint8_t p;
 } Request;
+
+typedef struct {
+    uint8_t *hash;
+    uint64_t answer;
+} decoding;
 
 Request *create_empty_request(void);
 
