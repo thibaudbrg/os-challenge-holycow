7844a6ffead4637cf42e4742570a9d0b03568a43
onsriahi14 <73482468+onsriahi14@users.noreply.github.com>
2022-11-13 02:38:10 +0100

    Update decoder.h

--- include/decoder.h
+++ include/decoder.h
@@ -1,13 +1,15 @@
-#pragma once
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#ifndef CHALLENGE_DECODER_H
+#define CHALLENGE_DECODER_H
 
 #include "request.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-uint64_t decode(Request const *request);
+void print_SHA(const unsigned char *SHA);
+int compare(const uint8_t *to_compare, const Request *request);
+uint8_t *hash(uint64_t *to_hash);
+uint64_t decode(const Request *request, The_Hash *theHash );
 
-#ifdef __cplusplus
-}
-#endif
+#endif //CHALLENGE_DECODER_H
