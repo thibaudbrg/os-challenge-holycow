b7da1312f14021027585b4e9101f91465a70f831
ghaliabennani <80267991+ghaliabennani@users.noreply.github.com>
2022-11-13 19:00:40 +0100

    multiprocess beginning

--- src/server.c
+++ src/server.c
@@ -8,6 +8,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #include "messages.h"
 #include "decoder.h"
@@ -15,13 +16,12 @@
 
 #define SOCKET_ERROR (-1)
 #define SERVER_BACKLOG 1024
-#define THREAD_POOL_SIZE 4
+#define PROCESS_NUMBER 2
 #define SA struct sockaddr
 #define SA_IN struct sockaddr_in
 
-pthread_t thread_pool[THREAD_POOL_SIZE];
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t condition_var = PTHREAD_COND_INITIALIZER; // Threads wait until something happens and then wake up
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 int check(int exp, char const *msg) {
     if (exp == SOCKET_ERROR) {
@@ -41,31 +41,30 @@ void compute_SHA(node_t const *const work) {
     }
 }
 
-_Noreturn void *thread_function(void *arg) {
-    Queue *queue = (Queue *) arg;
-    // Infinite loop because we never want these threads to die
-    while (1) {
-        node_t *work = NULL;
+void process_function(Queue* queue) {
+    node_t *work = NULL;
 
-        pthread_mutex_lock(&mutex);
+    //pthread_mutex_lock(&lock);
+    work = dequeue(queue);
+    //pthread_mutex_unlock(&lock);
+
+    while (work == NULL) {
+        //pthread_mutex_lock(&lock);
         work = dequeue(queue);
-        if (work == NULL) {
-            pthread_cond_wait(&condition_var, &mutex);
-            work = dequeue(queue);
-        }
-        pthread_mutex_unlock(&mutex);
-
-        if (work != NULL) {
-            // We have a connection
-            compute_SHA(work);
-
-            // We free the connfd and the corresponding request
-            destroy_node(work);
-            free(work);
-            work = NULL;
-        }
     }
-}
+    //pthread_mutex_unlock(&lock);
+
+
+    //We have a connection
+    compute_SHA(work);
+
+    // We free the connfd and the corresponding request
+    destroy_node(work);
+    free(work);
+    work = NULL;
+
+    }
+
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -88,11 +87,7 @@ int main(int argc, char *argv[]) {
     int addrlen = sizeof(servaddr);
 
     Queue *queue = createQueue();
-
-    // First off, create a bunch of threads to handle future connections
-    for (int i = 0; i < THREAD_POOL_SIZE; ++i) {
-        pthread_create(&thread_pool[i], NULL, thread_function, queue);
-    }
+    pid_t pid[PROCESS_NUMBER];
 
     check((sockfd = socket(AF_INET, SOCK_STREAM, 0)), "Socket creation failed...");
     printf("Socket successfully created.\n");
@@ -113,8 +108,9 @@ int main(int argc, char *argv[]) {
     check(listen(sockfd, SERVER_BACKLOG), "Listening failed...");
     printf("Server listening.\n");
 
-
+    int i = 0;
     while (1) {
+
         // Accept the data packet from client
         check(connfd = accept(sockfd, (SA *) (struct sockaddr *) &servaddr, (socklen_t *) &addrlen),
               "Server accept failed...");
@@ -123,9 +119,23 @@ int main(int argc, char *argv[]) {
         int *p_connfd = malloc(sizeof(int));
         *p_connfd = connfd;
 
-        pthread_mutex_lock(&mutex);
-        enqueue(p_connfd, queue);
-        pthread_cond_signal(&condition_var); // Wake up the thread
-        pthread_mutex_unlock(&mutex);
+
+
+        int pid_c = 0;
+        if ((pid_c = fork())==0){
+           // pthread_mutex_lock(&lock);
+            enqueue(p_connfd, queue);
+            //pthread_mutex_unlock(&lock);
+            process_function(queue);
+        }
+        else{
+            pid[i++] = pid_c;
+            if( i >= PROCESS_NUMBER-1){
+                i = 0;
+                while(i < PROCESS_NUMBER)
+                waitpid(pid[i++], NULL, 0);
+                i = 0;
+            }
+        }
     }
 }
